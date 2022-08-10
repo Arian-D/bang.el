a5fa3727b66fb39bc3eebc461da9a5b2659d6c78
Arian-D <ArianXDehghani@gmail.com>
2022-08-10 13:45:34 -0700

    Added more to the README, and fixed annotation formatting

--- bang.el
+++ bang.el
@@ -1,20 +1,25 @@
 (defcustom !bangs
-  '(("w" . ("wikipedia" . "https://en.wikipedia.org/w/index.php?search=%s"))
+  '(("w" . ("Wikipedia" . "https://en.wikipedia.org/w/index.php?search=%s"))
+    ("a" . ("Amazon" . "https://www.amazon.com/s?k=%s")) 
     ("imdb" . ("IMDB" . "https://www.imdb.com/find?s=all&q=%s")))
   "A list of bangs")
 
 (defun bang--annotate (b)
-  (cadr
-   (assoc b !bangs)))
+  (format "\t%s"
+	  (cadr
+	   (assoc b !bangs))))
 
 (defun ! ()
-  "DuckDuckGo-style bangs"
+  "Pick a bang from `!bangs', enter search keyword, and open in
+`browse-url-browser-function'."
   (interactive)
   (let* ((completion-extra-properties
-	 (list :annotation-function
-	       #'bang--annotate))
+	  (list :annotation-function
+		#'bang--annotate))
 	 (bang (completing-read "❕" !bangs))
 	 (template-url (cddr (assoc bang !bangs)))
 	 (search (read-string "> "))
 	 (url (format template-url search)))
     (browse-url url)))
+
+(provide 'bang)
